a355697e8fc12bbd9e43240b11df38a7fe5b5f16
BernardoTobon <bernardotobon1@gmail.com>
2024-01-18 13:38:18 -0500

    create component

---
+++ src/Components/Loging.js
@@ -0,0 +1,11 @@
+const Login = () => {
+    return(
+        <div className="grid grid-cols-1 lg:grid-cols-2 relative">
+            <div className="">
+
+            </div>
+        </div>
+    )
+}
+
+export default Login
