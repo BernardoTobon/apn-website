31b20f486f900c7dd7b5b1422973bf0904938a09
BernardoTobon <bernardotobon1@gmail.com>
2024-02-20 13:36:03 -0500

    update code

--- src/Components/data/Diameters.js
+++ src/Components/data/Diameters.js
@@ -32,121 +32,6 @@ const Diameters = () => {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2">
-      <div className="pl-2 md:pl-64 pt-8">
-        <form
-          onSubmit={handleSubmit((data) => {
-            console.log(data);
-          })}
-        >
-          <div>
-            <div className="m-1">
-              <label htmlFor="biacromial" className="text-D-G">
-                Biacromial
-              </label>
-            </div>
-            <input
-              {...register("biacromial", { required: true })}
-              className="m-1 text-center w-40 md:w-64 focus:outline-none border border-2 border-F-G rounded-lg"
-              value={biacromial}
-              type="text"
-              id="biacromial"
-              onChange={(e) => setBiacromial(e.target.value)}
-              onClick={() => {
-                handleFieldClick("biacromial");
-                handleFieldClickParagraph("biacromial");
-              }}
-            />
-            <div className="m-1">
-              <label htmlFor="biiliocrestal" className="text-D-G">
-                Biiliocrestal
-              </label>
-            </div>
-            <input
-              {...register("biiliocrestal", { required: true })}
-              className="m-1 text-center w-40 md:w-64 focus:outline-none border border-2 border-F-G rounded-lg"
-              value={biiliocrestal}
-              type="text"
-              id="biiliocrestal"
-              onChange={(e) => setBiiliocrestal(e.target.value)}
-              onClick={() => {
-                handleFieldClick("biiliocrestal");
-                handleFieldClickParagraph("biiliocrestal");
-              }}
-            />
-            <div className="m-1">
-              <label htmlFor="longitudDelPie" className="text-D-G">
-                Longitud del pie
-              </label>
-            </div>
-            <input
-              {...register("longitudDelPie", { required: true })}
-              className="m-1 text-center w-40 md:w-64 focus:outline-none border border-2 border-F-G rounded-lg"
-              value={footLenght}
-              type="text"
-              id="longitudDelPie"
-              onChange={(e) => setFootLengt(e.target.value)}
-              onClick={() => {
-                handleFieldClick("longitudDelPie"),
-                  handleFieldClickParagraph("longitudDelPie");
-              }}
-            />
-            <div className="m-1">
-              <label htmlFor="transversoDelTorax" className="text-D-G">
-                Transverso del toráx
-              </label>
-            </div>
-            <input
-              {...register("tranversoDelTorax", { required: true })}
-              className="m-1 text-center w-40 md:w-64 focus:outline-none border border-2 border-F-G rounded-lg"
-              value={transverseThorax}
-              type="text"
-              id="transversoDelTorax"
-              onChange={(e) => setTransverseTorax(e.target.value)}
-              onClick={() => {
-                handleFieldClick("transversoDelTorax"),
-                  handleFieldClickParagraph("tranversoDelTorax");
-              }}
-            />
-          </div>
-          <div className="m-1">
-            <label htmlFor="anteroposteriorDelTorax" className="text-D-G">
-              Anteroposterior del tórax
-            </label>
-          </div>
-          <input
-            {...register("anteroposteriorDelTorax", { required: true })}
-            className="m-1 text-center w-40 md:w-64 focus:outline-none border border-2 border-F-G rounded-lg"
-            value={anteroposteriorThorax}
-            type="text"
-            id="anteroposteriorDelTorax"
-            onChange={(e) => setAnteroPosteriorThorax(e.target.value)}
-            onClick={() => {
-              handleFieldClick("anteroposteriorDelTorax"),
-                handleFieldClickParagraph("anteroposteriorDelTorax");
-            }}
-          />
-          <div className="m-1">
-            <label htmlFor="humero" className="text-D-G">
-              Húmero
-            </label>
-          </div>
-          <input
-            {...register("humero", { required: true })}
-            className="m-1 text-center w-40 md:w-64 focus:outline-none border border-2 border-F-G rounded-lg"
-            value={humerus}
-            type="text"
-            id="humero"
-            onChange={(e) => setHumerus(e.target.value)}
-            onClick={() => {
-              handleFieldClick("humero"), handleFieldClickParagraph("humero");
-            }}
-          />
-          <input
-            type="submit"
-            className="text-D-G bg-T-G rounded-full w-28 h-10 mt-4 hover:bg-F-G hover:text-white"
-          />
-        </form>
-      </div>
       <div className="flex flex-col">
         {videoVisible && (
           <div className="flex justify-center pt-4">
@@ -166,6 +51,8 @@ const Diameters = () => {
                   ? "/Video2.mp4"
                   : videoVisible === "humero"
                   ? "/Video2.mp4"
+                  : videoVisible === "biestiloideoDeLaMuñeca"
+                  ? "/Video1.mp4"
                   : videoVisible
               }
               autoPlay
@@ -185,7 +72,9 @@ const Diameters = () => {
               derecho.
             </p>
           ) : paragraphVisible === "longitudDelPie" ? (
-            <p>Es la distacia entre los puntos anterior y posterior del pie.</p>
+            <p>
+              Es la distancia entre los puntos anterior y posterior del pie.
+            </p>
           ) : paragraphVisible === "transversoDelTorax" ? (
             <p>
               Es la distancia entre los puntos más laterales del tórax. (a nivel
@@ -199,13 +88,167 @@ const Diameters = () => {
           ) : paragraphVisible === "humero" ? (
             <p>
               Es la distancia entre el epicóndilo y la epitróclea del húmero, el
-              codo deberá estar en un ángulo de 90°<br/>
+              codo deberá estar en un ángulo de 90°
+              <br />
               <b>Se usa para cálculo del somatotipo</b>
+            </p>
+          ) : paragraphVisible === "biestiloideoDeLaMuñeca" ? (
+            <p>
+              Es la distancia entre la apófisis estiloides del radio y del
+              cúbito. El sujeto debe estar con el antebrazo en pronación con una
+              flexión de muñeca de 90°.
+              <br />
+              <b>Se usa para cálculo de la masa ósea.</b>
             </p>
           ) : (
             paragraphVisible
           )}
         </div>
+      </div>
+      <div className="pl-2 md:pl-64 pt-8">
+        <form
+          onSubmit={handleSubmit((data) => {
+            console.log(data);
+          })}
+        >
+          <div className="grid grid-cols-2 md:grid-cols-1">
+            <div className="m-1">
+              <div>
+                <label htmlFor="biacromial" className="text-D-G">
+                  Biacromial
+                </label>
+              </div>
+              <input
+                {...register("biacromial", { required: true })}
+                className="m-1 text-center w-16 md:w-64 focus:outline-none border border-2 border-F-G rounded-lg"
+                value={biacromial}
+                type="text"
+                id="biacromial"
+                onChange={(e) => setBiacromial(e.target.value)}
+                onClick={() => {
+                  handleFieldClick("biacromial");
+                  handleFieldClickParagraph("biacromial");
+                }}
+              />
+            </div>
+            <div className="m-1">
+              <div>
+                <label htmlFor="biiliocrestal" className="text-D-G">
+                  Biiliocrestal
+                </label>
+              </div>
+              <input
+                {...register("biiliocrestal", { required: true })}
+                className="m-1 text-center w-16 md:w-64 focus:outline-none border border-2 border-F-G rounded-lg"
+                value={biiliocrestal}
+                type="text"
+                id="biiliocrestal"
+                onChange={(e) => setBiiliocrestal(e.target.value)}
+                onClick={() => {
+                  handleFieldClick("biiliocrestal");
+                  handleFieldClickParagraph("biiliocrestal");
+                }}
+              />
+            </div>
+            <div className="m-1">
+              <div>
+                <label htmlFor="longitudDelPie" className="text-D-G">
+                  Longitud del pie
+                </label>
+              </div>
+              <input
+                {...register("longitudDelPie", { required: true })}
+                className="m-1 text-center w-16 md:w-64 focus:outline-none border border-2 border-F-G rounded-lg"
+                value={footLenght}
+                type="text"
+                id="longitudDelPie"
+                onChange={(e) => setFootLengt(e.target.value)}
+                onClick={() => {
+                  handleFieldClick("longitudDelPie"),
+                    handleFieldClickParagraph("longitudDelPie");
+                }}
+              />
+            </div>
+            <div className="m-1">
+              <div>
+                <label htmlFor="transversoDelTorax" className="text-D-G">
+                  Transverso del tórax
+                </label>
+              </div>
+              <input
+                {...register("transversoDelTorax", { required: true })}
+                className="m-1 text-center w-16 md:w-64 focus:outline-none border border-2 border-F-G rounded-lg"
+                value={transverseThorax}
+                type="text"
+                id="transversoDelTorax"
+                onChange={(e) => setTransverseTorax(e.target.value)}
+                onClick={() => {
+                  handleFieldClick("transversoDelTorax"),
+                    handleFieldClickParagraph("transversoDelTorax");
+                }}
+              />
+            </div>
+            <div className="m-1">
+              <div>
+                <label htmlFor="anteroposteriorDelTorax" className="text-D-G">
+                  Anteroposterior del tórax
+                </label>
+              </div>
+              <input
+                {...register("anteroposteriorDelTorax", { required: true })}
+                className="m-1 text-center w-16 md:w-64 focus:outline-none border border-2 border-F-G rounded-lg"
+                value={anteroposteriorThorax}
+                type="text"
+                id="anteroposteriorDelTorax"
+                onChange={(e) => setAnteroPosteriorThorax(e.target.value)}
+                onClick={() => {
+                  handleFieldClick("anteroposteriorDelTorax"),
+                    handleFieldClickParagraph("anteroposteriorDelTorax");
+                }}
+              />
+            </div>
+            <div className="m-1">
+              <div>
+                <label htmlFor="humero" className="text-D-G">
+                  Húmero
+                </label>
+              </div>
+              <input
+                {...register("humero", { required: true })}
+                className="m-1 text-center w-16 md:w-64 focus:outline-none border border-2 border-F-G rounded-lg"
+                value={humerus}
+                type="text"
+                id="humero"
+                onChange={(e) => setHumerus(e.target.value)}
+                onClick={() => {
+                  handleFieldClick("humero"),
+                    handleFieldClickParagraph("humero");
+                }}
+              />
+            </div>
+          </div>
+          <div className="m-1">
+            <div>
+              <label htmlFor="biestiloideoDeLaMuñeca"
+              className="text-D-G">Biestiloideo de la muñeca</label>
+            </div>
+            <input {...register("biestiloideoDelaMuñeca")}
+            className="m-1 text-center w-16 md:w-64 focus:outline-none border border-2 border-F-G rounded-lg"
+            value={wristBistyloid}
+            type="text"
+            id="biestiloideoDeLaMuñeca"
+            onChange={(e) => setWristBistyloid(e.target.value)}
+            onClick={() => {
+              handleFieldClick("biestiloideoDeLaMuñeca"),
+              handleFieldClickParagraph("biestiloideoDeLaMuñeca")
+            }}
+            />
+          </div>
+          <input
+            type="submit"
+            className="text-D-G bg-T-G rounded-full w-28 h-10 mt-4 hover:bg-F-G hover:text-white"
+          />
+        </form>
       </div>
     </div>
   );
