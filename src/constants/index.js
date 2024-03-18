557eae8ad607a1a54bda8545906640333320eab6
BernardoTobon <bernardotobon1@gmail.com>
2024-03-18 11:40:00 -0500

    added constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,3 +8,38 @@ export const GENDER = [
     label: "Mujer",
   },
 ];
+
+export const BLOOD_TYPE = [
+  {
+    value: 'A+',
+    label: 'A+',
+  },
+  {
+    value: 'A-',
+    label: 'A-',
+  },
+  {
+    value: 'B+',
+    label: 'B+',
+  },
+  {
+    value: 'B-',
+    label: 'B-',
+  },
+  {
+    value: 'O+',
+    label: 'O+',
+  },
+  {
+    value: 'O+',
+    label: 'O-',
+  },
+  {
+    value: 'AB+',
+    label: 'AB+',
+  },
+  {
+    value: 'AB-',
+    label: 'AB-',
+  },
+]
